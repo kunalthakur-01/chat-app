3fc9db40b4473ed6314fb0b3e2243b46b571f04e
kunal <13kunalthakur@gmail.com>
2023-03-12 10:43:26 +0530

    adding the auth routes

--- server/routes/userAuthRoutes.js
+++ server/routes/userAuthRoutes.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const User = require('../models/userModel');
-const httpError = require('../models/httpErrorModel');
+const express = require("express");
+const User = require("../models/userModel");
+const httpError = require("../models/httpErrorModel");
 
 const router = express.Router();
 
-router.post('/login', async (req, res, next) => {
-    const {userName, email, image, password} = req.body;
-    let existingUser;
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
 
-    try{
-        existingUser = User.find({email});
-    } catch(err) {
-        const error = new HttpError('Login failed!', 500);
-        return next(error);
-    }
+  try {
+    existingUser = User.find({ email });
+  } catch (err) {
+    const error = new HttpError("Login failed!", 500);
+    return next(error);
+  }
 
-    if(!existingUser) {
-        const error = new httpError("User does'nt exist, create an account", 401);
-        return next(error);
-    }
+  if (!existingUser) {
+    const error = new httpError("User does'nt exist, create an account", 401);
+    return next(error);
+  }
 
-    if(existingUser.password !== password){
-        const error = new httpError("Invalid credentials!", 401);
-        return next(error);
-    }
+  if (existingUser.password !== password) {
+    const error = new httpError("Invalid credentials!", 401);
+    return next(error);
+  }
+
+  res.status(200).json({ user: existingUser });
 });
 
-router.post('/signup', async (req, res, next) => {
-
+router.post("/signup", async (req, res, next) => {
+    const { userName, email, image, password } = req.body;
 });
 
 module.exports = router;
