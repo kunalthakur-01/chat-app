1104f4a58fcb46dc0dd2b13f3fa815da7928dd0e
kunal <13kunalthakur@gmail.com>
2023-03-15 00:00:30 +0530

    added body parser

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
+const MONGODB_URI =
+  "mongodb+srv://kunal-thakur:kunal25@cluster0.bfzhgmk.mongodb.net/chatting-app?retryWrites=true&w=majority";
+
 const express = require("express");
+const mongoose = require("mongoose");
 const userAuthRoute = require("./routes/userAuthRoutes");
+const userMessageBoxRoute = require("./routes/userMessageBoxRoute");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/api/auth", userAuthRoute);
+
+app.use("/api/message", userMessageBoxRoute);
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
@@ -13,4 +25,12 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(3000);
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("connected");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
