a899ceaa2b7da42339ff2189474ae9ab54d49384
kunal <13kunalthakur@gmail.com>
2023-03-23 13:30:33 +0530

    user mesage box

---
+++ client/src/component/chat-box/message-box/MessageBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from "./MessageBox.module.scss";
+
+const MessageBox = () => {
+  return (
+    <div className={`${classes.message_box_section} box_container`}></div>
+  )
+}
+
+export default MessageBox;
