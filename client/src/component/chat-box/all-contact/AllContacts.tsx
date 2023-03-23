0d358e055b0a97554ce1e344a959216dfd7fb4dd
kunal <13kunalthakur@gmail.com>
2023-03-23 13:30:49 +0530

    user's contacts

---
+++ client/src/component/chat-box/all-contact/AllContacts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from "./AllContacts.module.scss";
+
+const AllContacts = () => {
+  return (
+    <div className={`${classes.all_contacts_section} box_container`}></div>
+  )
+}
+
+export default AllContacts;
