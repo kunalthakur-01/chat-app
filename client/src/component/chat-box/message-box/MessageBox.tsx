5fc00c17f856c160710a4b09c0dc4daef16ebcd3
kunal <13kunalthakur@gmail.com>
2023-03-26 16:22:50 +0530

    adding styles

--- client/src/component/chat-box/message-box/MessageBox.tsx
+++ client/src/component/chat-box/message-box/MessageBox.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import classes from "./MessageBox.module.scss";
+import MessageBoxDetail from "./MessageBoxDetail";
 
 const MessageBox = () => {
   return (
-    <div className={`${classes.message_box_section} box_container`}></div>
-  )
-}
+    <div className={`${classes.message_box_section} box_container`}>
+      <MessageBoxDetail />
+      <hr className={classes.message_box_section_hr} />
+    </div>
+  );
+};
 
 export default MessageBox;
