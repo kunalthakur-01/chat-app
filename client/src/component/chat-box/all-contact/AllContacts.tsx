12c488de1531211bfa7357aa1539048527a9f701
kunal <13kunalthakur@gmail.com>
2023-03-26 16:24:00 +0530

    designing contact component

--- client/src/component/chat-box/all-contact/AllContacts.tsx
+++ client/src/component/chat-box/all-contact/AllContacts.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 import classes from "./AllContacts.module.scss";
 
-const AllContacts = () => {
+import { FiSearch } from "react-icons/fi";
+import PinnedMessages from "./PinnedMessages";
+import AllMessages from "./AllMessages";
+
+const AllContacts: React.FC = () => {
   return (
-    <div className={`${classes.all_contacts_section} box_container`}></div>
-  )
-}
+    <div className={`${classes.all_contacts_section} box_container`}>
+      <div className={classes.top_section}>
+        <h1>Messages</h1>
+        <button>
+          <FiSearch />
+        </button>
+      </div>
+      <div className={classes.all_messages}>
+        <PinnedMessages />
+        <AllMessages />
+      </div>
+    </div>
+  );
+};
 
 export default AllContacts;
