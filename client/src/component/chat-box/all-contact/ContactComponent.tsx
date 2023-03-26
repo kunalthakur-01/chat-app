6f4314d1ba514b390fbae6ab2412a42027c34059
kunal <13kunalthakur@gmail.com>
2023-03-26 16:21:04 +0530

    user contact component

---
+++ client/src/component/chat-box/all-contact/ContactComponent.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classes from "./ContactComponent.module.scss";
+
+const ContactComponent: React.FC = () => {
+  return (
+    <li className={classes.Contact_component}>
+      <div className={classes.contact_detail_section}>
+        <img
+          src="https://i.pinimg.com/originals/23/c1/03/23c103fcf8312f7b905c42fe421caf5f.jpg"
+          alt="contact"
+        />
+        <div className={classes.contact_detail}>
+          <h3>Kunal Thakur</h3>
+          <p>hello bro</p>
+        </div>
+      </div>
+      <div className={classes.message_status}>
+        <time>12:30 AM</time>
+        <span>2</span>
+      </div>
+    </li>
+  );
+};
+
+export default ContactComponent;
