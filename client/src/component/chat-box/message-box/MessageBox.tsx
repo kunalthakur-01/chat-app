e6d815346f09740a2363073b68fe695eb0a2c997
kunal <13kunalthakur@gmail.com>
2023-03-27 23:31:05 +0530

    split the code into the components

--- client/src/component/chat-box/message-box/MessageBox.tsx
+++ client/src/component/chat-box/message-box/MessageBox.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import classes from "./MessageBox.module.scss";
 import MessageBoxDetail from "./MessageBoxDetail";
+import MessagesArea from "./message-area/MessagesArea";
+import SendMessageForm from "./SendMessageForm";
 
 const MessageBox = () => {
   return (
     <div className={`${classes.message_box_section} box_container`}>
       <MessageBoxDetail />
       <hr className={classes.message_box_section_hr} />
+      <MessagesArea />
+      <SendMessageForm />
     </div>
   );
 };
