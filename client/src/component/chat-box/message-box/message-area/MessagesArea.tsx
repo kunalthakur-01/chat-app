02c3f9f818cad37aa684502b15cd7d913a4069dc
kunal <13kunalthakur@gmail.com>
2023-05-03 11:13:03 +0530

    splitting the senders and recievers messages

--- client/src/component/chat-box/message-box/message-area/MessagesArea.tsx
+++ client/src/component/chat-box/message-box/message-area/MessagesArea.tsx
@@ -7,8 +7,34 @@ import SenderMessages from './SenderMessages';
 const MessagesArea = () => {
   return (
     <div className={classes.messages_area}>
-      <SenderMessages />
-      <ReceiverMessages />
+      {/* <SenderMessages />
+      <ReceiverMessages /> */}
+      <div className={classes.allMessages}>
+        <div className={`${classes.message} ${classes.left}`}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate aspernatur quae aperiam rem illo non?</p>
+          <span>9:40 PM</span>
+        </div>
+        <div className={`${classes.message} ${classes.right}`}>
+          <p>kidaa</p>
+          <span>9:40 PM</span>
+        </div>
+        <div className={`${classes.message} ${classes.right}`}>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, fuga!</p>
+          <span>9:40 PM</span>
+        </div>
+        <div className={`${classes.message} ${classes.left}`}>
+          <p>hello world ha bol li krda.. m bs betha hoya h si vehla</p>
+          <span>9:40 PM</span>
+        </div>
+        <div className={`${classes.message} ${classes.left}`}>
+          <p>hello world Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, eveniet.</p>
+          <span>9:40 PM</span>
+        </div>
+        <div className={`${classes.message} ${classes.left}`}>
+          <p>bs vdia</p>
+          <span>9:40 PM</span>
+        </div>
+      </div>
     </div>
   )
 }
