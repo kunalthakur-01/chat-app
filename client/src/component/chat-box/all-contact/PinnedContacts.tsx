baa73dd6ff9fafffab8a20989ebe46d4ca04ea53
kunal <13kunalthakur@gmail.com>
2023-05-03 11:29:10 +0530

    pinned contacts component

---
+++ client/src/component/chat-box/all-contact/PinnedContacts.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import classes from "./PinnedContacts.module.scss";
+
+import { TbPinnedFilled } from "react-icons/tb";
+import ContactComponent from "./ContactComponent";
+
+const PinnedContacts: React.FC = () => {
+  return (
+    <div className={classes.pinned_section}>
+      <p>
+        <TbPinnedFilled /> Pinned Message
+      </p>
+      <ul className={classes.pinned_messages_list}>
+        <ContactComponent />
+        <ContactComponent />
+        <ContactComponent />
+      </ul>
+    </div>
+  );
+};
+
+export default PinnedContacts;
