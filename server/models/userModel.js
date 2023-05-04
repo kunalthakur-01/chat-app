0410d0eab8deb0510149a40f4e753b059adde7b4
kunal <13kunalthakur@gmail.com>
2023-05-04 17:59:58 +0530

    modify user model little bit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,32 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  messageBox: {
+    type: Schema.Types.ObjectId,
+    ref: "Usermessagebox",
+    required: true
+  },
+  contacts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    phone: {
-        type: Number,
-        required: true,
-        unique: true
+  ],
+  pinnedContacts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    image: {
-        type: String
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    contacts: [Schema.Types.ObjectId],
-    pinnedContacts: [Schema.Types.ObjectId]
+  ],
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
