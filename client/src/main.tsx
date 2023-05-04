b76a3a48752df5974f5679c3fb4f38a897181786
kunal <13kunalthakur@gmail.com>
2023-05-04 17:57:21 +0530

    wrap context provider over the application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import UserContextProvider from "./context/UserContext";
+import ContactContextProvider from "./context/ContactContext";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <UserContextProvider>
+    <ContactContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContactContextProvider>
+  </UserContextProvider>
 );
