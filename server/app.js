1ae6ca763b04762f4bcf5619418b0377a51259a6
kunal <13kunalthakur@gmail.com>
2023-05-06 13:35:11 +0530

    added CORS for send request from client to server

--- server/app.js
+++ server/app.js
@@ -14,6 +14,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-width, Content-Type, Accept, Authorization"
+  );  
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});           
+
 app.use("/api/auth", userAuthRoute);
 
 app.use("/api/message", userMessageBoxRoute);
