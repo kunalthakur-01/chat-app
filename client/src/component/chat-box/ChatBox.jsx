9c9e2664306d8fa567dcfadc5f736319fc838635
kunal <13kunalthakur@gmail.com>
2023-05-10 23:03:41 +0530

    rendering chatbox dynamically

--- client/src/component/chat-box/ChatBox.jsx
+++ client/src/component/chat-box/ChatBox.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import classes from "./ChatBox.module.scss";
 
-import { FaBeer } from "react-icons/fa";
 import AdminPanel from "./admin-panel/AdminPanel";
 import AllContacts from "./all-contact/AllContacts";
 import MessageBox from "./message-box/MessageBox";
