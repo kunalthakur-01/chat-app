ff649feaa311312c2b768e540c081e4d66d78f26
kunal <13kunalthakur@gmail.com>
2023-05-10 23:04:12 +0530

    renderd pinned contacts if any

--- client/src/component/chat-box/all-contact/PinnedContacts.jsx
+++ client/src/component/chat-box/all-contact/PinnedContacts.jsx
@@ -5,16 +5,14 @@ import classes from "./PinnedContacts.module.scss";
 import { TbPinnedFilled } from "react-icons/tb";
 import ContactComponent from "./ContactComponent";
 
-const PinnedContacts = () => {
+const PinnedContacts = (props) => {
   return (
     <div className={classes.pinned_section}>
       <p>
         <TbPinnedFilled /> Pinned Message
       </p>
       <ul className={classes.pinned_messages_list}>
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
+        {props.contacts.map(contact => <ContactComponent key={contact._id} contact={contact} />)}
       </ul>
     </div>
   );
