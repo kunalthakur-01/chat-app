d37e6dd11b003a574f6ccf2259c1e0c7d97ea226
kunal <13kunalthakur@gmail.com>
2023-05-10 23:02:41 +0530

    auth apis

--- client/src/api/auth-apis/AuthApi.js
+++ client/src/api/auth-apis/AuthApi.js
@@ -16,7 +16,7 @@ export const signup = async (body) => {
 };
 
 export const login = async (body) => {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch('http://localhost:5000/api/auth/login', {
         method: "POST",
         body: JSON.stringify(body),
         headers: {
