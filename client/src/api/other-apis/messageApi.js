60001e624069edec0dbecbff7b772a114b7d41de
kunal <13kunalthakur@gmail.com>
2023-05-23 23:14:47 +0530

    get message api

--- client/src/api/other-apis/messageApi.js
+++ client/src/api/other-apis/messageApi.js
@@ -14,3 +14,14 @@ export const sendMessage = async(body) => {
 
     return data;
 };
+
+export const getMessages = async({user1, user2}) => {
+    const response = await fetch(`http://localhost:5000/api/message/get/${user1}/${user2}`);
+
+    const data = await response.json();
+    console.log(data)
+
+    if(!response.ok) throw new Error(data.message);
+
+    return data;
+};
