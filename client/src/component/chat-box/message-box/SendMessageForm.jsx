5cbe7c67f2c7b074a970834b36136405664a9750
kunal <13kunalthakur@gmail.com>
2023-05-25 21:02:08 +0530

    done with sending message to others

--- client/src/component/chat-box/message-box/SendMessageForm.jsx
+++ client/src/component/chat-box/message-box/SendMessageForm.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import classes from "./SendMessageForm.module.scss";
 
 import { IoSendSharp } from "react-icons/io5";
+import useHttp from "../../../hooks/http-hook";
+import { sendMessage } from "../../../api/other-apis/messageApi";
+import { UserContextManage } from "../../../context/UserContext";
+import { useSearchParams } from "react-router-dom";
 
-const SendMessageForm = () => {
+const SendMessageForm = (props) => {
+  const { sendRequest, data, error, status } = useHttp(sendMessage);
+  const getUserByQueryParam = useSearchParams()[0].get("user");
+  const [message, setMessage] = useState();
+  const ctx = useContext(UserContextManage);
+
+  const SendMessageForm = (e) => {
+    e.preventDefault();
+    message && sendRequest({senderId: ctx.userData._id, receiverId: getUserByQueryParam, message, time: new Date().toISOString()});
+
+    setMessage("");
+  }
+
   return (
-    <form className={classes.send_message_form}>
-      <input type="text" placeholder="Write a message" />
-      <button>
+    <form onSubmit={SendMessageForm} className={classes.send_message_form}>
+      <textarea value={message} onChange={(e) => setMessage(e.target.value)} rows={1} type="text" placeholder="Write a message" />
+      <button type="submit">
         <IoSendSharp />
       </button>
     </form>
