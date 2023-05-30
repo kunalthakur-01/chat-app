273d32d9ba8854579a90b3dc1856b3e6f601c5c9
kunal <13kunalthakur@gmail.com>
2023-05-30 22:39:59 +0530

    established client socket connection

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatBox from "../component/chat-box/ChatBox";
 
+import openSocket from "socket.io-client";
+
 const Chat = () => {
+  useEffect(() => {
+    openSocket("http://localhost:5000");
+  }, []);
   return (
     <>
       <ChatBox />
