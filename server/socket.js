aafc7ccc7b0d0844002a66c5f806298d2a65c9be
kunal <13kunalthakur@gmail.com>
2023-06-13 23:13:26 +0530

    initializing and getting IO instance

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,7 @@
 let io;
 
+const onlineusers = {};
+
 module.exports = {
     init: (httpServer) => {
         io = require('socket.io')(httpServer, {
@@ -12,5 +14,17 @@ module.exports = {
     getIO: () => {
         if(!io) throw new Error('Socket.io not initialized');
         return io;
+    },
+    addOnlineUsers: (userId, socketId) => {
+      if(!io) throw new Error('Socket.io not initialized');
+      onlineusers[userId] = socketId;
+      // console.log(onlineusers)
+    },
+    onlineusers,
+    sendMessage: (to, data) => {
+      if(!io) throw new Error('Socket.io not initialized');
+      const sendUserSocket = onlineusers[to];
+      if(sendUserSocket) io.to(sendUserSocket).emit('msg-sent', data);
+      // console.log(sendUserSocket)
     }
 };
